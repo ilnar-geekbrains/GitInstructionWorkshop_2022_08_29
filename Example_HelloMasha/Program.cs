25de85cc5d2e073d15835863997f00326624fe97
lubovcha <chalovaljubov@mail.ru>
2022-08-29 01:43:35 +0300

    2

---
+++ Example_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя полььзователя");
+string username = Console.Readline();
+
+if(username == "Маша");
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
